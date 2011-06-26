79aece1a4fdaaa84a168fd8b66dca8c7fd38c146
ruby <zpingliu@gmail.com>
2011-06-25 19:43:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orchid::Application.config.secret_token = 'fd579a44025fedb0229bb5d6cc8735283230a8cf0d0bba3d64ac490199b3c54924b78ec130faade918e635efecdeb86207f87619555c6227d0974f69363ccc57'
